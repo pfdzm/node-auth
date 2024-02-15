b78b287b62e4bf570b3fbb6fdde8b168ecaa3e11
Pablo Fernandez Michel <pfdzm@users.noreply.github.com>
2024-02-15 12:20:12 +0100

    read port from env vars

--- server/src/index.js
+++ server/src/index.js
@@ -110,6 +110,7 @@ app.post("reset-pw-through-security-questions", (req, res) => {
   password = newPassword;
   res.send("Password reset");
 });
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+const PORT = process.env.PORT ?? 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
